80eace160078565941141566da8c71a45c9d3dfc
Mert Demirok <mertdemirok80@gmail.com>
2019-12-04 13:35:44 +0100

    Instantiate book to the console

--- app.js
+++ app.js
@@ -11,52 +11,54 @@ class Book {
 // UI Class: Handle UI Tasks
 
 class UI {
-    static displayBooks () {
-        const storedBooks = [
-            {
-                title: 'Book One', 
-                author: 'John Doe',
-                isbn:'123456'
-            },
-            {
-                title: 'Book Two', 
-                author: 'Susan Doe',
-                isbn:'123789'
-            }
+  static displayBooks() {
+    const storedBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "123456"
+      },
+      {
+        title: "Book Two",
+        author: "Susan Doe",
+        isbn: "123789"
+      }
+    ];
+    const books = storedBooks;
+    books.forEach(book => UI.addBookToList(book));
+  }
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
 
-        ];
-        const books = storedBooks;
-        books.forEach((book) => UI.addBookToList(book));
-    }
-    static addBookToList(book){
-        const list = document.querySelector('#book-list');
+    const row = document.createElement("tr");
 
-        const row = document.createElement('tr');
-
-        row.innerHTML = `
+    row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
             <td><a href='#' class = 'btn btn-danger btn-sm delete'>X</a></td>
         `;
-        list.appendChild(row);
-    }
+    list.appendChild(row);
+  }
 }
 
 // Store Class:  Handles Storage
 
 // Event: Display Books
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a book
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-    // Get form values
-    const title = document.getElementById('#title').value;
-    const author = document.getElementById('#author').value;
-    const isbn = document.getElementById('#isbn').value;
+document.querySelector("#book-form").addEventListener("submit", e => {
+  // Get form values
+  const title = document.getElementById("#title").value;
+  const author = document.getElementById("#author").value;
+  const isbn = document.getElementById("#isbn").value;
 
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+  console.log(book);
 });
 
 // Event: Remove a book
